eeb2072813fde76062c7d6095afef7bfee0b7fe0
weeqeecombe50 <mynieagbona@hotmail.com>
2025-08-15 13:30:51 +0300

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Willkommen zum Git Kollaborationstool</h1>
+                <p>Hier können Sie Git-Repositorys analysieren und visualisieren.</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
